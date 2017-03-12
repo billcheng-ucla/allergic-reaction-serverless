7d0d0f1a9fc3795b3a819e66ccad107b21636458
Kevin Ferron <overfocus@outlook.com>
2017-03-12 14:41:06 -0700

    comment out rc

--- restaurantCard.js
+++ restaurantCard.js
@@ -33,32 +33,32 @@ module.exports = React.createClass({
   // when everything has loaded
   render: function() {
 
-    return (
-      <div className='well pcard' onClick={(e) => this.props.handleClick(e)} >
-      <div className='row'>
-          <div className='col-xs-4'>
-              <img src='https://media-cdn.tripadvisor.com/media/photo-s/05/49/6b/df/le-grill-restaurant-and.jpg'
-              width={'100%'}
-              height={'100%'} />
-          </div>
-          <div className='col-xs-8'>
-          <h3>Restaurant name</h3>
-          <hr />
-          <h5> Promotion name </h5>
-          <h6 className='text-muted'> Promotion description </h6>
-          <h6 className='text-muted'> Active until blah </h6>
-          </div>
-      </div>
-      </div>
-  )
-    //
-    // return div(null,
-    //
+  //   return (
+  //     // <div className='well pcard' onClick={(e) => this.props.handleClick(e)} >
+  //     // <div className='row'>
+  //     //     <div className='col-xs-4'>
+  //     //         <img src='https://media-cdn.tripadvisor.com/media/photo-s/05/49/6b/df/le-grill-restaurant-and.jpg'
+  //     //         width={'100%'}
+  //     //         height={'100%'} />
+  //     //     </div>
+  //     //     <div className='col-xs-8'>
+  //     //     <h3>Restaurant name</h3>
+  //     //     <hr />
+  //     //     <h5> Promotion name </h5>
+  //     //     <h6 className='text-muted'> Promotion description </h6>
+  //     //     <h6 className='text-muted'> Active until blah </h6>
+  //     //     </div>
+  //     // </div>
+  //     // </div>
+  // //)
+  //   //
+     return div(null,
+
     //   button({onClick: this.handleClick, disabled: this.state.disabled}, 'Add Item'),
     //
-    //   ul({children: this.state.items.map(function(item) {
-    //     return li(null, item)
-    //   })})
+       ul({children: this.state.items.map(function(item) {
+         return li(null, item)
+       })})
 
     )
   },
